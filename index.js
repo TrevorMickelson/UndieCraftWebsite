7e53266f41fd3997ab396bd453157e125e088f0f
Trevor <tmick28@protonmail.com>
2022-03-14 23:58:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Registering tooltips
+ */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+function copyFunction() {
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+}
